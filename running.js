ce0e1784ebe577d18bedd439d74e1d21c842c207
roger18gm <roger18gm@gmail.com>
2024-04-30 15:52:23 -0600

    javascript practice

---
+++ running.js
@@ -0,0 +1,12 @@
+const apples = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total);
+
+const myApples = 4;
+const friendApples = 2;
+let totalApples = myApples + friendApples;
+
+document.getElementById("myApplesElement").textContent = myApples;
+document.getElementById("friendApplesElement").textContent = friendApples;
+document.getElementById("totalApples").textContent = totalApples;
